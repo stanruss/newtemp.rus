3019d1ee5758724792c059c0aac211baa55d9b73
Stanruss <stan19781@gmail.com>
2017-06-06 21:28:46 +0300

    5_mod carousel

--- assets/templates/Stas/app/js/common.js
+++ assets/templates/Stas/app/js/common.js
@@ -18,8 +18,15 @@ $(function() {
 	});
 $(".owl-carousel").owlCarousel({
   	animateOut: 'fadeOut',
+  	animateIn: 'fadeIn',
     nav: true,
+    navText: ["<img src='../assets/templates/Stas/app/img/left.png'>","<img src='../assets/templates/Stas/app/img/right.png'>"],
     items:1,
+    autoplay:true,
+		autoplayTimeout:5000,
+		loop:true,
+    margin:100,
+		autoplayHoverPause:false,
     smartSpeed:2000
   });
 	});
